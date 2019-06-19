6ab8133a8520618f65402705d66eef3227b7562b
mdamian9 <m.damian9@hotmail.com>
2019-06-18 20:43:40 -0700

    Created orders.js in routes folder for /orders endpoint requests

---
+++ api/routes/orders.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET orders'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'POST orders (order created)'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET orderId',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'DELETE orderId',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router;

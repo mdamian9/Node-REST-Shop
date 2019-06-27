c294edac42584f49d8f541e2c22fce945da02823
mdamian9 <m.damian9@hotmail.com>
2019-06-26 23:51:22 -0700

    Created order model / added to orders.js routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET orders'
+    Order.find().exec().then(docs => {
+        res.status(200).json(docs);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'POST orders (order created)',
-        createdOrder: order
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order.save().then(result => {
+        res.status(201).json({
+            message: 'POST orders (order created)',
+            createdOrder: result
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
